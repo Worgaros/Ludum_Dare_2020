7000bb17ab701e7690701f3fae00a9bf69613cef
William <williamp10022000@gmail.com>
2020-04-18 23:42:56 +0200

    Sorting layer oxygen

--- Assets/Scripts/Player/PlayerOxygen.cs
+++ Assets/Scripts/Player/PlayerOxygen.cs
@@ -33,10 +33,8 @@ public class PlayerOxygen : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("oxygenBubble"))
         {
-<<<<<<< Updated upstream
             currentOxygen += 1f;
-=======
->>>>>>> Stashed changes
+
             Score.GetOxygenPoint();
             currentOxygen += 0.5f;
         }
