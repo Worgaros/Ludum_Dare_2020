5d9a92ddd69d3e8e0c55da6a4710b4fdfb5a2dbb
Bryan <bryancrim@gmail.com>
2020-04-18 04:39:41 +0200

    Script EnnemyFollow added

---
+++ Assets/Scripts/EnnemyFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemyFollow : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    private Rigidbody2D body;
+
+    private int playerDistance = 10;
+    [SerializeField] float speed = 1f;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        var direction = Vector3.Distance(transform.position, player.transform.position);
+
+        if (direction < playerDistance)
+        {
+            body.velocity = (player.transform.position - transform.position).normalized * speed;
+        }
+    }
+    
+}
