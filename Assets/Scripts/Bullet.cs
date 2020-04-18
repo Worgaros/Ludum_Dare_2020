979bab934eeb4af1558367958767b7557a69c0c4
Marvin <marvin.k.scharding@gmail.com>
2020-04-18 04:55:47 +0200

    updated player prefab

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    PolygonCollider2D bulletCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletCollider = GetComponent<PolygonCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
