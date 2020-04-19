0c777befaff97525a3697cb209f76cbd362ca1ac
Marvin <marvin.k.scharding@gmail.com>
2020-04-19 13:57:56 +0200

    Created script to work with oxygen bottles to attach and recharge player

--- Assets/Scripts/Player/PlayerOxygen.cs
+++ Assets/Scripts/Player/PlayerOxygen.cs
@@ -9,6 +9,8 @@ public class PlayerOxygen : MonoBehaviour
 
     OxygenDisplay oxygen;
 
+    bool takeFromBottle = false;
+
     private void Start()
     {
         oxygen = FindObjectOfType<OxygenDisplay>();
@@ -27,10 +29,39 @@ public class PlayerOxygen : MonoBehaviour
         }
         
         currentOxygen -= Time.deltaTime;
-        Debug.Log("oxygen" + currentOxygen);
+       // Debug.Log("oxygen" + currentOxygen);
         oxygen.setOxygen(currentOxygen, maxOxygen);
+
+        TakeOxygenFromBottle();
+
+        if(currentOxygen>=maxOxygen)
+        {
+            currentOxygen = maxOxygen;
+        }
     }
     
+     void TakeOxygenFromBottle()
+    {
+        if (takeFromBottle)
+        {
+            while (currentOxygen < maxOxygen)
+            {
+                currentOxygen += 2 * Time.deltaTime;
+            }
+        }
+        
+    }
+
+    public void ConnectingToBottle()
+    {
+        takeFromBottle = true;
+    }
+
+    public void DisconnectedFromBottle()
+    {
+        takeFromBottle = false;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "oxygenBubble")
---
+++ Assets/Scripts/World/OxygenCable.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OxygenCable : MonoBehaviour
+{
+
+    Rigidbody2D body;
+    Transform playerPosition;
+    [SerializeField] float cableSpeed = 5;
+    bool canAttach = false;
+    int numberOfUse = 0;
+    [SerializeField] GameObject oxygenBottle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        playerPosition = FindObjectOfType<PlayerController>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(canAttach)
+        {
+            AttachToPlayer();
+        }
+        if(numberOfUse>=3)
+        {
+            Destroy(oxygenBottle);
+        }
+    }
+
+    void AttachToPlayer()
+    {
+        body.velocity = (playerPosition.transform.position - transform.position).normalized * cableSpeed;
+        //if(Vector3.Distance(transform.position,playerPosition.transform.position)>=2)
+        //{
+        //    canAttach = false;
+        //}
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("player"))
+        {
+            canAttach = true;
+            numberOfUse++;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("player"))
+        {
+            canAttach = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("player"))
+        {
+            Debug.Log("collided");
+            PlayerOxygen playerOxygen;
+            playerOxygen = collision.gameObject.GetComponent<PlayerOxygen>();
+            playerOxygen.ConnectingToBottle();
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("player"))
+        {
+            Debug.Log("collided");
+            PlayerOxygen playerOxygen;
+            playerOxygen = collision.gameObject.GetComponent<PlayerOxygen>();
+            playerOxygen.DisconnectedFromBottle();
+        }
+    }
+}
