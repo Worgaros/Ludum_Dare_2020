72d71abf168b26dd6cae38ddb223b0a6081b5dcf
Bryan <bryancrim@gmail.com>
2020-04-19 21:00:34 +0200

    MiniMap added

---
+++ Assets/Scripts/TargetIndicator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetIndicator : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    static bool indicator = false;
+
+    private void Start()
+    {
+        indicator = false;
+    }
+
+    void Update()
+    {
+        var direction = target.position - transform.position;
+
+        var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    public static void ActivateIndicator()
+    {
+        indicator = true;
+    }
+
+    public static void DesactivateIndicator()
+    {
+        indicator = false;
+    }
+}
