068219420f5adc9b25e30a0db1a916c8057e84e4
Bryan <bryancrim@gmail.com>
2020-04-20 00:30:57 +0200

    Fix EnnemyBullet

--- Assets/Scripts/Enemies/EnnemyBullet.cs
+++ Assets/Scripts/Enemies/EnnemyBullet.cs
@@ -33,7 +33,7 @@ public class EnnemyBullet : MonoBehaviour
         {
             body.velocity = (mechano.gameObject.transform.position - transform.position).normalized * speed;
         }
-        Destroy(gameObject, 5f); ;
+        Destroy(gameObject, 2f); ;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
